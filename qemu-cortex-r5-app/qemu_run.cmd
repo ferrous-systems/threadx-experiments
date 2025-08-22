47c8e304b27e060c04c52ec612c471ce1966ea7d
Jonathan Pallant <jonathan.pallant@ferrous-systems.com>
2025-08-22 18:35:01 +0100

    Add missing license text.

--- qemu-cortex-r5-app/qemu_run.cmd
+++ qemu-cortex-r5-app/qemu_run.cmd
@@ -3,6 +3,9 @@
 : # set as the cargo runner and needs to work on Windows
 : # and POSIX platforms. See https://stackoverflow.com/questions/17510688
 : # for details.
+: #
+: # SPDX-FileCopyrightText: Copyright (c) 2025 Ferrous Systems
+: # SPDX-License-Identifier: CC0-1.0
 :; ./qemu_run.sh $* ; exit
 @ECHO OFF
 call qemu_run.bat %*
