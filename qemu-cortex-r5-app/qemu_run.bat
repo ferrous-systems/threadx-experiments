f281470247782d5d11e0010e22e164b9d090d7b4
Jonathan Pallant (Ferrous Systems) <jonathan.pallant@ferrous-systems.com>
2025-08-22 17:47:45 +0100

    Now runs on Windows 11

--- qemu-cortex-r5-app/qemu_run.bat
+++ qemu-cortex-r5-app/qemu_run.bat
@@ -6,10 +6,10 @@ rem SPDX-License-Identifier: CC0-1.0
 rem This requires you to previously run `cargo install defmt-print`
 
 set ELF_BINARY=%1
-set MACHINE="-cpu cortex-r5f -machine versatileab"
-set LOG_FORMAT='{[{L}]%bold} {s} {({ff}:{l:1})%dimmed}'
-echo "ELF_BINARY=$ELF_BINARY"
-echo "Running on '$MACHINE'..."
-echo "------------------------------------------------------------------------"
-qemu-system-arm %MACHINE% -semihosting-config enable=on,target=native -kernel %* | defmt-print -e %ELF_BINARY% --log-format="%LOG_FORMAT%"
+set MACHINE=-cpu cortex-r5f -machine versatileab
+set QEMU_PATH=%ProgramFiles%\qemu
+echo ELF_BINARY=%ELF_BINARY%
+echo Running on '%MACHINE%'...
+echo ------------------------------------------------------------------------
+"%QEMU_PATH%\qemu-system-arm" %MACHINE% -semihosting-config enable=on -nographic -kernel %* | defmt-print -e %ELF_BINARY%
 echo "------------------------------------------------------------------------"
