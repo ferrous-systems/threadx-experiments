55cb98923a6999d7e93a01d27b245f78409d2d5e
Jonathan Pallant (Ferrous Systems) <jonathan.pallant@ferrous-systems.com>
2025-08-22 18:08:29 +0100

    defmt logging fixes for Windows
    Either defmt-semihosting or defmt-print on Windows doesn't like
    logs that don't have their types specified.

--- qemu-cortex-r5-app/qemu_run.bat
+++ qemu-cortex-r5-app/qemu_run.bat
@@ -11,5 +11,5 @@ set QEMU_PATH=%ProgramFiles%\qemu
 echo ELF_BINARY=%ELF_BINARY%
 echo Running on '%MACHINE%'...
 echo ------------------------------------------------------------------------
-"%QEMU_PATH%\qemu-system-arm" %MACHINE% -semihosting-config enable=on -nographic -kernel %* | defmt-print -e %ELF_BINARY%
-echo "------------------------------------------------------------------------"
+"%QEMU_PATH%\qemu-system-arm" %MACHINE% -semihosting-config enable=on -nographic -kernel %* | defmt-print -e %ELF_BINARY% --log-format="{[{L}]%%bold} {s} {({ff}:{l:1})%%dimmed}"
+echo "------------------------------------------------------------------------
